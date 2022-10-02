8581df10052239497976e39515eadf228dc25afa
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-02 17:55:36 +0300

    Popup Now Works with JS

--- frontend/scripts/profile.js
+++ frontend/scripts/profile.js
@@ -1,1 +1,11 @@
-console.log("Batata");
+const editProfileButton = document.getElementById("edit-profile");
+const closeEditProfile = document.getElementById("close-edit");
+const editProfilePopup = document.querySelector(".popup");
+
+editProfileButton.addEventListener("click", () => {
+    editProfilePopup.style.display = "flex";
+});
+
+closeEditProfile.addEventListener("click", () => {
+    editProfilePopup.style.display = "none";
+});
