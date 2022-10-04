0e80dfe68f6444a7ddde278761572e1c8a00ad3f
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-04 18:41:30 +0300

    Added Script to Feed

---
+++ frontend/scripts/feed.js
@@ -0,0 +1,29 @@
+const logOut = document.getElementById("logout-button");
+const token = window.localStorage.getItem("token");
+const baseURL = "http://127.0.0.1:8000/api";
+
+registerURL = baseURL + "/feed";
+
+// Functions
+
+const postAPI = async (api_url, api_data, api_token = null) => {
+    try {
+        return await axios.post(
+            api_url,
+            api_data,
+            {
+                headers: { Authorization: `Bearer ${api_token}`}
+            }
+
+        ) .then(function (response) {
+            console.log(response)});
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+// Main
+
+
+data = {};
+postAPI(registerURL, data, token);
